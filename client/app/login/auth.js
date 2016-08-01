cca5be12785775c8768f43518f43ff8590e96222
El_Gato <albertomadueno@gmail.com>
2016-08-01 16:06:11 -0700

    map and persisting user data

--- client/app/login/auth.js
+++ client/app/login/auth.js
@@ -1,3 +1,4 @@
+
 angular.module('NeighborPool.Login',[])
 
 .config(function($stateProvider){
@@ -7,8 +8,14 @@ angular.module('NeighborPool.Login',[])
 	.state('login', {
 		url: '/login',
 		templateUrl: 'app/login/login.html',
+		userService: function($http){
+			// return $http.get('signup');
+		},
 		controller: function($scope, $http, $location, $window){
-
+			$scope.signin = function(){
+				// var user1 = new User({username: $scope.user.username, password: $scope.user.password});
+				// console.log(user1);
+			}
 		},
 		controllerAs: 'LoginCtrl'
 	});
--- client/app/signup/auth.js
+++ client/app/signup/auth.js
@@ -1,18 +1,29 @@
-angular.module('NeighborPool.Signup', [])
+angular.module('NeighborPool.Signup',[])
 
 .config(function($stateProvider){
-	$stateProvider
 
-		.state('signup', {
-			url: '/signup',
-			templateUrl: 'app/signup/signup.html',
-			resolve: {
-				controller: function($scope, $http, $location, $window){
-					$scope.username = '';
-					$scope.password = '';
-				}
+	$stateProvider
+	// the state will go on the html that you want route the user.
+	.state('signup', {
+		url: '/signup',
+		templateUrl: 'app/signup/signup.html',
+		userService: function($http){
+			// return $http.POST('signup');
+		},
+		controller: function($scope, $http, $location, $window){
+			$scope.saveUser = function(){
+				
+				
+				$http({
+					method: 'POST',
+					url: '/signup',
+					data: $scope.user
+				})
 			}
-		})
-});
+		},
+		controllerAs: 'LoginCtrl'
+	});
+
+})
 
 
