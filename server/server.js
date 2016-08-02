06d4468fcf02fcfe425d3b4134a6fa5ed95a9e66
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-08-02 14:47:01 -0700

    Merge branch 'pr/28' into Dev
    # Conflicts:
    #	client/app/app.js
    #	client/index.html

--- server/server.js
+++ server/server.js
@@ -14,17 +14,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
 
-
-app.post('/signin', function(req, res){
-	app.json('hello world')
+app.post('/login', function(req, res){
+	res.json('hello world')
 });
 
+app.post('/signup', function(req, res){
+	res.send('testing signup');
+	User.create(req.body, function(err, data){
+		console.log(data);
+	});
+})
+
 app.get('/userProfile', function(req, res){
-	app.send('hello world')
+	res.send('hello world')
 });
 
 app.get('/home', function(req, res){
-	app.json('hello world')
+	res.json('hello world')
 });
 
 app.get('/mapView', function(req, res){
