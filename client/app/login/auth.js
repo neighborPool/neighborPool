a91feab6f39643886c22ed11c8a19d5104a67cfb
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-08-02 16:37:40 -0700

    merged logins

--- client/app/login/auth.js
+++ client/app/login/auth.js
@@ -1,27 +1,6 @@
 
 angular.module('NeighborPool.Login',[])
 .config(function($stateProvider){
-<<<<<<< HEAD
-
-	$stateProvider
-	// the state will go on the html that you want route the user.
-	.state('login', {
-		url: '/login',
-		templateUrl: 'app/login/login.html',
-		userService: function($http){
-			// return $http.get('signup');
-		},
-		controller: function($scope, $http, $location, $window){
-			$scope.signin = function(){
-				// var user1 = new User({username: $scope.user.username, password: $scope.user.password});
-				// console.log(user1);
-			}
-		},
-		controllerAs: 'LoginCtrl'
-	});
-
-})
-=======
     $stateProvider
     // the state will go on the html that you want route the user.
     .state('login', {
@@ -44,5 +23,4 @@ angular.module('NeighborPool.Login',[])
         },
         controllerAs: 'LoginCtrl'
     });
-});
->>>>>>> pr/31
+})
